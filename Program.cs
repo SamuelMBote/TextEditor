68b30c4f0b5a09f02432576d74d99c4b543d1e89
Samuel MBote <36938470+SamuelMBote@users.noreply.github.com>
2023-01-19 21:19:27 -0300

    Aprendendo a criar um Editor de Texto, Typeguards que aprendi no curso de TS

---
+++ Program.cs
@@ -0,0 +1,71 @@
+﻿using System.Xml.Linq;
+using System.IO;
+using System;
+class Program
+{
+  public static void Main(string[] args)
+  {
+    Menu();
+  }
+  static void Menu()
+  {
+    Console.Clear();
+    Console.WriteLine(value: "O que você deseja fazer?");
+    Console.WriteLine(value: "1 - Abrir arquivo");
+    Console.WriteLine(value: "2  - Criar novo arquivo");
+    Console.WriteLine(value: "0 - Sair");
+
+    short option = short.Parse(s: Console.ReadLine());
+
+    switch (option)
+    {
+      case 0: System.Environment.Exit(exitCode: 0); break;
+      case 1: Abrir(); break;
+      case 2: Editar(); break;
+      default: Menu(); break;
+    }
+  }
+  static void Abrir()
+  {
+    Console.Clear();
+    Console.WriteLine("Qual o caminho do arquivo?");
+    string path = Console.ReadLine();
+    if (path != null)
+      using (var file = new StreamReader(path: path))
+      {
+        string text = file.ReadToEnd();
+        Console.WriteLine(value: text);
+      }
+    Console.WriteLine("");
+    Console.ReadLine();
+    Menu();
+
+  }
+  static void Editar()
+  {
+    Console.Clear();
+    Console.WriteLine(value: "Digite seu texto abaixo (ESC para sair)");
+    Console.WriteLine(value: "-----------------------");
+    string text = "";
+    do
+    {
+      text += Console.ReadLine();
+      text += Environment.NewLine;
+    }
+    while (Console.ReadKey().Key != ConsoleKey.Escape);
+    Salvar(text: text);
+  }
+  static void Salvar(string text)
+  {
+    Console.Clear();
+    Console.WriteLine(value: "Qual caminho para salvar o arquivo?");
+    var path = Console.ReadLine();
+    if (path != null)
+      using (var file = new StreamWriter(path: path))
+      {
+        file.Write(value: text);
+      }
+    Console.WriteLine(value: $"Arquivo {path} salvo com sucesso!");
+    Menu();
+  }
+}
